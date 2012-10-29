1c465c8caa8dcc63af2f16e68f9aeb189add96ee
Alexis Mignon <alexis.mignon@gmail.com>
2012-10-29 17:12:42 +0100

    Changed Version number and license name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
 	name = "flickr_api",
-	version = '0.2',
+	version = '0.3',
 	description = "Python wrapper for the Flickr API",
 	author = "Alexis Mignon",
 	author_email = "alexis.mignon@gmail.com",
 	url = "http://code.google.com/p/python-flickr-api/",
 	packages = ["flickr_api"],
-	license = "GPL 2.0",
+	license = "BSD License",
 )
 
